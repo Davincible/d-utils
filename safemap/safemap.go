5456e1d7928f56dcdef2730e8c8ad4951f122159
Davincible <david.brouwer.99@gmail.com>
2024-12-11 21:52:02 +0100

    fix: safemap

--- safemap/safemap.go
+++ safemap/safemap.go
@@ -2,6 +2,8 @@
 package safemap
 
 import (
+	"fmt"
+	"strconv"
 	"sync"
 )
 
@@ -33,13 +35,15 @@ func (m *Map[T]) Get(key string) (T, bool) {
 // GetOrSet retrieves the value associated with the given key from the map.
 // It returns the value and a boolean indicating whether the key was present.
 // Only if it not set yet it will assign the new value.
-func (m *Map[T]) GetOrSet(key string, val T) (T, bool) {
+func (m *Map[T]) GetOrSet(key any, val T) (T, bool) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
-	valN, ok := m.items[key]
+	k := getKey(key)
+
+	valN, ok := m.items[k]
 	if !ok {
-		m.items[key] = val
+		m.items[k] = val
 	}
 
 	return valN, ok
@@ -74,10 +78,10 @@ func (m *Map[T]) GetMap() map[string]T {
 }
 
 // Set stores the value associated with the given key in the map.
-func (m *Map[T]) Set(key string, value T) {
+func (m *Map[T]) Set(key any, value T) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
-	m.items[key] = value
+	m.items[getKey(key)] = value
 }
 
 // SetMap replaces the internal map with the  provided map.
@@ -89,11 +93,11 @@ func (m *Map[T]) SetMap(n map[string]T) {
 }
 
 // Delete removes the value associated with the given key from the map.
-func (m *Map[T]) Delete(key string) {
+func (m *Map[T]) Delete(key any) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
-	delete(m.items, key)
+	delete(m.items, getKey(key))
 }
 
 // Length returns the number of key-value pairs in the map.
@@ -115,4 +119,46 @@ func (m *Map[T]) Keys() []string {
 	}
 
 	return keys
+}
+
+// getKey takes any value and returns a string representation that can be used as a key.
+func getKey(value any) string {
+	switch v := value.(type) {
+	case nil:
+		return "nil"
+	case string:
+		return v
+	case int:
+		return strconv.Itoa(v)
+	case int8:
+		return strconv.FormatInt(int64(v), 10)
+	case int16:
+		return strconv.FormatInt(int64(v), 10)
+	case int32:
+		return strconv.FormatInt(int64(v), 10)
+	case int64:
+		return strconv.FormatInt(v, 10)
+	case uint:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint8:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint16:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(v), 10)
+	case uint64:
+		return strconv.FormatUint(v, 10)
+	case float32:
+		return strconv.FormatFloat(float64(v), 'f', -1, 32)
+	case float64:
+		return strconv.FormatFloat(v, 'f', -1, 64)
+	case bool:
+		return strconv.FormatBool(v)
+	case []byte:
+		return string(v)
+	case error:
+		return v.Error()
+	default:
+		return fmt.Sprintf("%+v", v)
+	}
 }
